41d15b04672436db57166b61c4561b4f08ee70be
eatonk <eatonk@uoguelph.ca>
2014-04-24 14:01:28 -0400

    no more JSON parse profile

--- src/com/mingle/MainActivity.java
+++ src/com/mingle/MainActivity.java
@@ -1,8 +1,5 @@
 package com.mingle;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
@@ -13,7 +10,6 @@ import com.parse.ParseUser;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -50,31 +46,20 @@ public class MainActivity extends Activity {
 					@Override
 					public void onCompleted(GraphUser user, Response response) {
 						if(user != null) {
-							JSONObject userProfile = new JSONObject();
-							try {
-								/* Gets all the information that we need from Facebook and stores
-								 * it into a ParseUser that we can access later. */
-								userProfile.put("facebookId", user.getId());
-								userProfile.put("firstName", user.getFirstName());
-								if(user.getLocation().getProperty("name") != null) {
-									userProfile.put("location", (String) user.getLocation().getProperty("name"));
-								}
-								if(user.getProperty("gender") != null) {
-									userProfile.put("gender", (String) user.getProperty("gender"));
-								}
-								if(user.getBirthday() != null) {
-									userProfile.put("birthday", user.getBirthday());
-								}
-								
-								ParseUser currentUser = ParseUser.getCurrentUser();
-								currentUser.put("facebookId", user.getId());
-								currentUser.put("name", user.getName());
-								currentUser.put("profile", userProfile);
-								currentUser.saveInBackground();
-								
-							} catch (JSONException e) {
-								Log.d(MingleApplication.TAG, "Error pasing returned user data.");
+							ParseUser currentUser = ParseUser.getCurrentUser();
+							currentUser.put("facebookId", user.getId());
+							currentUser.put("firstName", user.getFirstName());
+							if(user.getProperty("gender") != null) {
+								currentUser.put("gender", (String) user.getProperty("gender"));
 							}
+							if(user.getLocation().getProperty("name") != null) {
+								currentUser.put("location", (String) user.getLocation().getProperty("name"));
+							}
+							if(user.getBirthday() != null) {
+								currentUser.put("birthday", user.getBirthday());
+							}
+							currentUser.put("mingling", false);
+							currentUser.saveInBackground();
 						}
 					}
 				});
